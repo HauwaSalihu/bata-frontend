f0523c2e97933180b67779cd8200ad1d197d0694
HauwaSalihu <hauwa.salihu@aun.edu.ng>
2024-12-17 23:00:33 +0100

    made url update

--- src/utils/make_request.js
+++ src/utils/make_request.js
@@ -4,65 +4,67 @@ import { startLoading, stopLoading } from "./slicers/loadingSlice";
 import { addNotification } from "./slicers/notificationSlice";
 
 const makeRequest = async (config, method, endpoint, data, queryParams) => {
-  const config_data = {show_loader: true, show_error_status: true, ...config};
+  const config_data = { show_loader: true, show_error_status: true, ...config };
   if (config_data.show_loader) {
-      store.dispatch(startLoading());
+    store.dispatch(startLoading());
   }
-//  console.log(config,method,endpoint)
+  //  console.log(config,method,endpoint)
   try {
-      const url = `/${endpoint}`;
-      let axiosConfig = {
-          method,
-          url,
-          params: queryParams, // Query parameters
-          // headers configuration
-          headers: data instanceof FormData 
-              ? { 'Content-Type': 'multipart/form-data','ngrok-skip-browser-warning': 'true' }
-              : { 'Content-Type': 'application/json','ngrok-skip-browser-warning': 'true' }
-      };
+    const url = `/${endpoint}`;
+    let axiosConfig = {
+      method,
+      url,
+      params: queryParams, // Query parameters
+      // headers configuration
+      headers:
+        data instanceof FormData
+          ? { "Content-Type": "multipart/form-data" }
+          : { "Content-Type": "application/json" },
+    };
 
     //   console.log(axiosConfig)
-      // If there's data, add it to config
-      if (data) {
-          axiosConfig.data = data;
-      }
+    // If there's data, add it to config
+    if (data) {
+      axiosConfig.data = data;
+    }
 
-      // console.log('Request config:', axiosConfig);
-      
-      // Make the request with the complete config
-      const response = await axiosInstance(axiosConfig);
+    // console.log('Request config:', axiosConfig);
 
-      const { status, message } = response.data;
+    // Make the request with the complete config
+    const response = await axiosInstance(axiosConfig);
 
-      if (status) {
-          return response.data;
-      } else {
-          if (config_data.show_error) {
-              store.dispatch(
-                  addNotification({
-                      type: "error",
-                      title: "Operation Failed!",
-                      description: message || "Request Failed.",
-                  })
-              );
-          }
-          return response.data;
-      }
-  } catch (error) {
+    const { status, message } = response.data;
+
+    if (status) {
+      return response.data;
+    } else {
       if (config_data.show_error) {
-          store.dispatch(
-              addNotification({
-                  type: "error",
-                  title: "Operation Failed!",
-                  description: error?.response?.data?.message || "Error handling request.",
-              })
-          );
+        store.dispatch(
+          addNotification({
+            type: "error",
+            title: "Operation Failed!",
+            description: message || "Request Failed.",
+          })
+        );
       }
-      return error?.response?.data || null;
+      return response.data;
+    }
+  } catch (error) {
+    if (config_data.show_error) {
+      store.dispatch(
+        addNotification({
+          type: "error",
+          title: "Operation Failed!",
+          description:
+            error?.response?.data?.message || "Error handling request.",
+        })
+      );
+    }
+    return error?.response?.data || null;
   } finally {
-      if (config_data.show_loader) {
-          store.dispatch(stopLoading());
-      }
+    if (config_data.show_loader) {
+      store.dispatch(stopLoading());
+    }
   }
 };
 
